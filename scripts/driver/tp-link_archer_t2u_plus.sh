16312278cdd3ae43cdd903a42ad31af7adcd8d3c
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-05-25 22:06:43 +0700

    Create tp-link_archer_t2u_plus.sh

---
+++ scripts/driver/tp-link_archer_t2u_plus.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Instal driver TP-LINK Archer T2U Plus
+
+if [[ $EUID -ne 0 ]]; then
+	echo "ERROR: Script ini harus dijalankan sebagai root!"
+	exit 1
+fi
+
+dependensi=(
+	"linux-headers-$(uname -r)"
+	"build-essential"
+	"bc"
+	"git"
+)
+
+apt-get update -y
+
+for d in "${dependensi[@]}"; do
+	apt-get install "${d}" -y
+done
+
+cd /usr/local/src
+git clone https://github.com/morrownr/8821au-20210708
+cd 8821au-20210708
+
+make -j$(nproc)
+make install
+modprobe 8821au
